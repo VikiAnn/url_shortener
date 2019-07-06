d97db8b0bc4c7d079eec98de06354c68d6434655
Viki Harrod <victoria.harrod@gmail.com>
2019-07-05 22:07:39 -0600

    Use curly brackets because do..end is subtly different

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :links
   end
- 
+
   # to disable the middleware, comment out the insert_after in config/application.rb
   # then:
-  
+
   # to use the proc, uncomment these
-  # get '/*hashid', as: :shorter_url, to: redirect do |params|
+  # get '/*hashid', as: :shorter_url, to: redirect { |params|
   #  link = Rails.cache.fetch(params[:hashid]) do
   #    Link.find_by!(hashid: params[:hashid])
   #  end
   #  link.uri
-  #end
-  
+  # }
+
   # to use the Rails Controller, uncomment this
   # get '/*hashid', to: 'redirect#index', as: :shorter_url
 end
